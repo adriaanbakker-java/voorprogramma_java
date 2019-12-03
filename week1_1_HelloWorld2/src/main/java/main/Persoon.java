03579bfc626612ba9c58ee58e6011575c05951c3
adriaanbakker_java <adriaan.bakker@hotmail.com>
2019-12-03 14:53:23 +0100

    added week 1 day 1

---
+++ week1_1_HelloWorld2/src/main/java/main/Persoon.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Persoon {
+	String Naam;
+	String Adres;
+	String Postcode;
+	int leeftijd;
+	
+	public Persoon(String naam, String adres, String postcode, int leeftijd) {
+		super();
+		Naam = naam;
+		Adres = adres;
+		Postcode = postcode;
+		this.leeftijd = leeftijd;
+	}
+}
